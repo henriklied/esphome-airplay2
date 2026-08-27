4c0ec1a26113af617b61be25418960afa06d24d2
Henrik Lied <henrik.lied@gmail.com>
2026-08-27 15:04:16 +0200

    chore: drop probe source from recursive-sources experiment

--- components/airplay_receiver/_exp/probe_mod.cpp
+++
@@ -1,6 +0,0 @@
-// probe: verify recursive_sources descends into one-level subdirs
-namespace airplay_receiver {
-namespace probe {
-int probe_value() { return 42; }
-}  // namespace probe
-}  // namespace airplay_receiver
